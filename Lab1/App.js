57b9dd00e09ddcf103b59b7f460b9e9db59b2057
hoangducbinh <hoangbinh02022002@gmail.com>
2023-09-16 23:12:05 +0700

    Fullversion

--- Lab1/App.js
+++ Lab1/App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Main from './src/Main';
 
+
 export default function App() {
   return (
     <View style={{...styles.container}}>
@@ -18,3 +19,8 @@ const styles = StyleSheet.create({
   },
 
 });
+
+
+
+  
+
--- Lab1/src/Main.js
+++ Lab1/src/Main.js
@@ -6,6 +6,10 @@ import Project2 from './Project2';
 import Project3 from './Project3';
 import Project4 from './Project4';
 import Project5 from './Project5';
+import Excersise2 from '../Excersise2/Excersise2';
+import Project6 from './Project6';
+import Project7 from './Project7';
+import Project8 from './Project8';
 
 
 
@@ -17,6 +21,10 @@ const Main = () => {
      { key: 'project3', title: 'project3', focusedIcon: 'history' },
      { key: 'project4', title: 'project4', focusedIcon: 'bell', unfocusedIcon: 'bell-outline' },
      {key : 'project5', title : 'project5', focusedIcon: 'account-music'},
+     {key : 'project6', title: 'peoject6', focusedIcon : 'access-point'},
+     {key : 'project7', title: 'peoject7', focusedIcon : 'airplane'},
+     {key : 'project8', title: 'peoject8', focusedIcon : 'alarm-light'},
+     //{key : 'Excersise2', title :'Excersise2', focusedIcon: 'account-music'},
 
   ]);
 
@@ -26,6 +34,10 @@ const Main = () => {
     project3: Project3,
     project4: Project4,
     project5: Project5,
+    project6: Project6,
+    project7: Project7,
+    project8: Project8,
+    //Excersise2 :Excersise2,
   });
 
   return (
